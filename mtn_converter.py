5fc97e574fb06e3be37c0995dedc812df41441ec
Colin Duffett <48289802+callinduffett@users.noreply.github.com>
2019-05-23 13:39:30 -0500

    Update mtn_converter.py

--- mtn_converter.py
+++ mtn_converter.py
@@ -1,3 +1,5 @@
+# this script should open a csv file that contains a list of .txt files in a directory,
+# then add a header row and convert the file to a csv and save it in the same folder
 import os
 import csv
 import os.path
